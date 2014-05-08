a4f3eec7f01d363aa7c0498d7d571ea881ccb5e5
dkhamsing <danielkhamsing@gmail.com>
2014-05-08 09:47:36 -0700

    Update color with rgb method

--- BrandColors/UIColor+BrandColors.m
+++ BrandColors/UIColor+BrandColors.m
@@ -294,7 +294,7 @@
 #pragma mark - Private
 
 + (UIColor*)dk_colorWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue {
-    return [UIColor colorWithRed:red/255 green:green/255 blue:blue/255 alpha:1];
+    return [UIColor colorWithRed:red/255.0f green:green/255.0f blue:blue/255.0f alpha:1.0f];
 }
 
 
